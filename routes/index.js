d54d8474ef1a12f4018b89f4287e612c8c34bdd0
Yan ZiQin <yanziqin04061994@126.com>
2017-11-11 18:39:27 -0800

    MongoDB

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var controllerMongoCollection = require('../controllers/database');
 
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+router.get('/getAllRoutes', controllerMongoCollection.getAllRoutes);
 module.exports = router;
